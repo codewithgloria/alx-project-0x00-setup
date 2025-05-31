3c7ba166074b873c10e84cb615a6165102846687
Gloria <abinezagloria@gmail.com>
2025-06-01 02:06:03 +0300

    added new files

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import React from 'react';
+
+const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Hello from index</h1>
-    </div>
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h1>Airbnb Application Clone system</h1>
+      <button className="border px-3 py-1 text-lg mt-3 bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
   );
-}
+};
+
+export default Home;
