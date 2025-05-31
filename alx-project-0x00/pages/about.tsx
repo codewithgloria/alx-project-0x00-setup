5781f122fa18b98b1a56a001d961fb5c2c69830e
Gloria <abinezagloria@gmail.com>
2025-06-01 02:14:46 +0300

    added landing and about pages

---
+++ alx-project-0x00/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-xl font-light">About Page</h1>
+    </div>
+  );
+};
+
+export default About;
